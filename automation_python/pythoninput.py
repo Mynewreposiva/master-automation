73ef48608c743b0e6883c938a8e9138178edb5a9
Siva automation <98083637+Mynewreposiva@users.noreply.github.com>
2026-09-10 15:36:53 +0530

    looks good

---
+++ automation_python/pythoninput.py
@@ -0,0 +1,14 @@
+
+# Python's input() function is used to take user input.
+# By default, it returns the user input in form of a string.
+name=input("Enter name")
+print("hello,", name)
+
+age=input("Enter age")
+print("your age is", age)
+
+# taking different inputs
+i =int(input("Enter number"))
+j=float(input("Enter another number"))
+
+print("your values are here", i, ",",j)
